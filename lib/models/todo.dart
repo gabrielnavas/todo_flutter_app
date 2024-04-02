5aef20c2d944a9433cd692e2aa7409331fed5f69
navas <gabrielnavas@email.com>
2024-04-02 20:32:50 -0300

    validate todo model

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:todo_app/models/todo_form.dart';
+
 class Todo {
   final String id;
   final String title;
@@ -14,4 +18,48 @@ class Todo {
     required this.updatedAt,
     this.completed = false,
   });
+
+  String? validate() {
+    String? titleError = validateTitle(title);
+    if (titleError != null) {
+      return titleError;
+    }
+    String? descriptionError = validateDescription(description);
+    if (descriptionError != null) {
+      return descriptionError;
+    }
+
+    return null;
+  }
+
+  static String? validateDescription(String description) {
+    if (description.isEmpty) {
+      return "Descrição deve ter no mínimo 1 caractere";
+    }
+    if (description.length > 100) {
+      return "Descrição deve ter no máximo 100 caracteres";
+    }
+
+    return null;
+  }
+
+  static String? validateTitle(String title) {
+    if (title.isEmpty) {
+      return "Título deve ter no mínimo 1 caractere";
+    }
+    if (title.length > 100) {
+      return "Título deve ter no máximo 100 caracteres";
+    }
+
+    return null;
+  }
+
+  static Todo from(TodoForm todoForm) {
+    return Todo(
+        id: Random().nextDouble().toString(),
+        title: todoForm.title,
+        description: todoForm.description,
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now());
+  }
 }
