4ab3c58b3dd275a6ca77581b01e3f2134d6e33ff
navas <gabrielnavas@email.com>
2024-04-02 19:12:24 -0300

    refactory te todo item widget

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -11,52 +11,83 @@ class TodoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const double iconSize = 25.0;
+    final width = MediaQuery.of(context).size.width;
 
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
+        borderRadius: BorderRadius.circular(15.0),
         color: Colors.white,
       ),
       margin: const EdgeInsets.only(left: 10, right: 10, top: 17),
+      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 5),
       child: ListTile(
-        trailing: const SizedBox(
-          width: 110,
-          child: Row(
-            children: [
-              Icon(
-                Icons.edit_outlined,
-                size: iconSize,
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              Icon(
-                Icons.remove_circle_outline_outlined,
-                size: iconSize,
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              Icon(
-                Icons.check_circle_outline_outlined,
-                size: iconSize,
-              )
-            ],
-          ),
-        ),
-        title: Text(
-          todo.title,
-          style: const TextStyle(
-            fontSize: 20,
-          ),
-        ),
-        subtitle: Text(
-          todo.description,
-          style: const TextStyle(
-              fontSize: 17, color: Colors.black87, fontWeight: FontWeight.w400),
-        ),
+        trailing: _trailingActions(width),
+        title: _title(),
+        subtitle: _subtitle(),
         onTap: () {},
+      ),
+    );
+  }
+
+  Text _subtitle() {
+    return Text(
+      todo.description,
+      style: const TextStyle(
+          fontSize: 17, color: Colors.black87, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Text _title() {
+    return Text(
+      todo.title,
+      style: const TextStyle(
+        fontSize: 20,
+      ),
+    );
+  }
+
+  SizedBox _trailingActions(double widthScreen) {
+    const double iconSize = 25.0;
+    const double spaceBetweenIcons = 10.0;
+    final widthSizedBox = widthScreen * .40;
+
+    return SizedBox(
+      width: widthSizedBox,
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.edit_outlined,
+              size: iconSize,
+            ),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith(
+                (states) => const Color.fromARGB(25, 193, 193, 193),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: spaceBetweenIcons,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.remove_circle_outline_outlined,
+              size: iconSize,
+            ),
+          ),
+          const SizedBox(
+            width: spaceBetweenIcons,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.check_circle_outline_outlined,
+              size: iconSize,
+            ),
+          ),
+        ],
       ),
     );
   }
